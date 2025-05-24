cc80ce9b1b9c12199ff2d9b2e1308b86700f5aa9
Madhan4r <madhan4r@gmail.com>
2025-05-24 12:54:31 +0530

    cache location

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import App from './App';
 import './index.scss';
 import 'bootstrap/dist/js/bootstrap.js';
 
+// ✅ Restore original path after 404 redirect from GitHub Pages
+if (sessionStorage.redirect) {
+  const redirectPath = sessionStorage.redirect;
+  delete sessionStorage.redirect;
+  window.history.replaceState(null, '', redirectPath);
+}
+
+
 ReactDOM.render(
   <React.Fragment>
     <App />
